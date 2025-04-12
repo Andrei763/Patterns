4da3921464c01106d7d98e1643426d5e10f0600f
Andrei <akifevandrej11@gmail.com>
2025-04-12 18:36:08 +0400

    commit artifacts

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,7 +1,14 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 class AuthTest {
 
 }
